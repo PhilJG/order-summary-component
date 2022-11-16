fa27a678f007ce421534d96592018fc406f8937d
Phil <phl.j.gray@gmail.com>
2022-11-15 19:28:44 -0800

    createRoot().render() added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 
-import Hero from '../components/Hero';
-import Main from '../components/Main';
-import Order from '../components/Order';
-import Pricing from '../components/Pricing';
+import Hero from '..components/Hero';
+import Main from '..components/Main';
+import Order from '..components/Order';
+import Pricing from '..components/Pricing';
 
 export default function App() {
-  return;
-  <div class="container">
-    <Hero />
-    <Main />
-    <Order />
-    <Pricing />
-  </div>;
+  return (
+    <div className="container">
+      <Hero />
+      <Main />
+      <Order />
+      <Pricing />
+    </div>
+  );
 }
-
-import './App.css';
