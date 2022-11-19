efcd16c1bdcc5df15f5992788de97671746f8c72
Phil <phl.j.gray@gmail.com>
2022-11-18 16:59:44 -0800

    script tag removed from index.html

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import Hero from '..components/Hero';
-import Main from '..components/Main';
-import Order from '..components/Order';
-import Pricing from '..components/Pricing';
+import Hero from './components/Hero';
+import Main from './components/Main';
+import Order from './components/Order';
+import Pricing from './components/Pricing';
 
 export default function App() {
   return (
@@ -12,6 +12,13 @@ export default function App() {
       <Main />
       <Order />
       <Pricing />
+      <div class="attribution">
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
     </div>
   );
 }
